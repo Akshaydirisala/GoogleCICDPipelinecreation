677266a808a1010afc58064b0444096f33035214
ADIRISA1 <adirisa1@ford.com>
2022-07-13 23:10:51 -0400

    seconf commit

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 from flask import Flask
-import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dirisala! last run'
+    return 'Hello Dirisala! last trail success'
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(debug=True)
