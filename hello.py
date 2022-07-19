b81a70d9279b6cf695e0508732ea4e8632ca0995
ADIRISA1 <adirisa1@ford.com>
2022-07-19 11:02:03 -0400

    second commit

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Team! Smart mobility FCSD'
+    return 'Hello Team! Smart mobility FCSD 07/19'
 
 if __name__ == '__main__':
     app.run(debug=True)
